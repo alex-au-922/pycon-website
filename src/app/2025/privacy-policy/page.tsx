a9b045930dd25290e67d8ad7b743898838ab1c1b
Alex Au <alexuau922@gmail.com>
2025-07-03 22:48:03 +0800

    Update styles for About, CallForActions, PrivacyPolicy, and Footer components

--- src/app/2025/privacy-policy/page.tsx
+++ src/app/2025/privacy-policy/page.tsx
@@ -11,7 +11,7 @@ import {
 
 export default async function PrivacyPolicy() {
   return (
-    <div className="bg-gradient-to-br from-blue-700 via-blue-500 to-blue-300 -mt-20 text-gray-600 text-sm md:text-base">
+    <div className="bg-gradient-to-br from-yellow-300 via-sky-500 to-sky-300 -mt-20 text-slate-500 text-base/7 md:text-lg/8">
       <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="bg-white bg-opacity-90 rounded-xl shadow-xl p-8 md:p-12 relative mt-20">
           <h1 className="font-bold text-2xl md:text-5xl mb-8 text-gray-800 text-center">
@@ -21,7 +21,7 @@ export default async function PrivacyPolicy() {
             <Introduction />
           </div>
 
-          <div className="space-y-8 text-gray-700 mt-8">
+          <div className="space-y-8 mt-8">
             <InformationWeCollect />
             <HowDoWeUseThePersonalInformationWeCollect />
             <WhoDoWeShareYourPersonalInformationWith />
